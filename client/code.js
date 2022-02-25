650c784a5589e1659b36e2c046750aef7f013bec
Zeno3463 <zenolim3463@gmail.com>
2022-02-25 18:05:14 +0800

    feat: Changed the word length to four

--- client/code.js
+++ client/code.js
@@ -1,12 +1,12 @@
 ////// VARIABLES //////
 let word = "";
-let answer = "CRANE";
+let answer = "";
 let listOfAlphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 ////// FUNCTIONS //////
 const updateWord = () => {
-	// reduce the word to five characters long
-	if (word.length > 5) word = word.slice(0, 5);
+	// reduce the word to four characters long
+	if (word.length > 4) word = word.slice(0, 4);
 
 	// display the word
 	$('.tile').each((i, obj) => {
@@ -17,12 +17,22 @@ const updateWord = () => {
 
 const submitWord = () => {
 	$.getJSON("dictionary.json", (dictionary) => {
-		if (!dictionary.includes(word.toLowerCase())) alert("That's not a word!");
+		// if the dictionary does not contain the submitted word
+		if (!dictionary.includes(word)) {
+			// show the 'that is not a word' pop up for 1 second
+			$(".that-is-not-a-word").html("<h1>That is not a word!</h1>");
+			setTimeout(() => $('.that-is-not-a-word').html(''), 1000);
+		}
 		else {
+			// loop through each letter in the word
 			for (let i = 0; i < word.length; i++) {
+				// if the letter matches the answer, shade it green
 				if (word[i] === answer[i]) $(`.tile:eq(0)`).addClass('tile-correct');
+				// if the letter is lower than the letter in the answer alphabetically, shade it yellow
 				else if (listOfAlphabets.indexOf(word[i]) < listOfAlphabets.indexOf(answer[i])) $(`.tile:eq(0)`).addClass('tile-yellow');
+				// if the letter is higher than the letter in the answer alphabetically, shade it red
 				else $(`.tile:eq(0)`).addClass('tile-red');
+				
 				$(`.tile:eq(0)`).removeClass('tile');
 			}
 			word = "";
@@ -58,3 +68,10 @@ const handleInput = (e) => {
 $('body').keyup((e) => handleInput(e.key));
 
 $('.key').each((_, obj) => $(obj).click(() => handleInput($(obj).text())))
+
+$(async () => {
+	await fetch("http://localhost:3000/rand").then(res => res.json()).then(res => {
+		console.log(res)
+		answer = res[0];
+	})
+});
